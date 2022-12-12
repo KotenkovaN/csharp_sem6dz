65e879c446c4719cc84c6d73c63ffeed3e26721c
Nataly <natalya-kotenkova@yandex.ru>
2022-12-12 13:01:16 +0100

    added task41

---
+++ task 41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear(); 
+Console.Write("Введите числа через пробел: "); 
+string[] n = Console.ReadLine().Split(" "); 
+int count = 0; 
+foreach (string i in n) 
+{ 
+          if (int.Parse(i) > 0) 
+          count++; 
+} 
+Console.WriteLine(count); 
